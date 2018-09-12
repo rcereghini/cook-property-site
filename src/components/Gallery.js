b2adc9ca3403ec93f8a35cee331f1a304888de03
Robert Cereghini <rcereghini@cleartitleaz.local>
2018-09-12 09:44:44 -0700

    Responsiveness

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -10,16 +10,18 @@ for (let i = 1; i < 51; i++)
 function Gallery(props) {
   return (
     <div className="container">
-      <iframe
-        class="video"
-        width="720"
-        height="405"
-        src="https://www.youtube.com/embed/nmP70Lwb_s4"
-        frameborder="0"
-        margin="20"
-        allow="autoplay; encrypted-media"
-        allowfullscreen
-      />
+      <div className="video-responsive">
+        <iframe
+          class="video"
+          width="720"
+          height="405"
+          src="https://www.youtube.com/embed/nmP70Lwb_s4"
+          frameborder="0"
+          margin="20"
+          allow="autoplay; encrypted-media"
+          allowfullscreen
+        />
+      </div>
       <p className="intro">{props.intro}</p>
       {IMAGES.map((image, i) => <img src={image} className="galleryImages" />)}
     </div>
